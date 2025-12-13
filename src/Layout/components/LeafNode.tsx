49f2564c266a65728ee3b9318b5294f5564e012e
Mustafa Koser <mustufakoser52@gmail.com>
2025-12-13 13:52:44 +0530

    Update LeafNode.tsx

--- src/Layout/components/LeafNode.tsx
+++ src/Layout/components/LeafNode.tsx
@@ -14,14 +14,21 @@ const LeafNode = memo(({ node, style, padding }: LeafNodeProps) => {
             className="bg-gray-900 flex items-center justify-center text-white font-mono text-sm overflow-hidden"
             style={{ ...style, padding: `${padding}px` }}
         >
-            <div className="flex justify-center p-0 border border-gray-700 rounded-2xl h-full w-full overflow-hidden ">
+            <div className="flex flex-col justify-center p-0 border border-gray-700 rounded-2xl h-full w-full overflow-hidden ">
                 
                 <div className="bg-gray-800 w-full text-center h-7 flex items-center justify-center border-b border-gray-700">
-                    
+
                     <span className="text-blue-400 font-semibold">
                         {node.component_connected || node.id}
                     </span>
 
+                </div>
+
+                <div className="flex p-2 text-xs text-gray-400 overflow-auto h-full w-full items-center justify-center">
+                    
+                    COMPONENT
+
+                    
                 </div>
 
             </div>
